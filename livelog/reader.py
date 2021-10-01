1b76dbbc844476219e509c66d82a9f00ae57c05a
PabloLec <pablo.lecolinet@gmail.com>
2021-10-01 11:44:11 +0200

    Add reader level filtering capability

--- livelog/reader.py
+++ livelog/reader.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, DirModifiedEvent, FileModifiedEvent
 from colorama import Style, Fore
-
+from .errors import *
 
 class Reader(FileSystemEventHandler):
     """Watchdog handler to monitor file changes."""
@@ -20,8 +20,10 @@ class Reader(FileSystemEventHandler):
         "INFO": Fore.BLUE,
         "DBUG": Fore.WHITE,
     }
+    _LONG_LEVEL_TO_SHORT = {"ERROR": "ERR!", "WARN": "WARNING", "INFO": "INFO", "DEBUG": "DBUG"}
+    _LEVELS = {"ERR!": 3, "WARN": 2, "INFO": 1, "DBUG": 0}
 
-    def __init__(self, file: str):
+    def __init__(self, file: str, level: str = "DEBUG"):
         """Reader initialization.
 
         Args:
@@ -30,6 +32,12 @@ class Reader(FileSystemEventHandler):
 
         self._file = file
         self._verify_file()
+
+        level = level.upper()
+        if level not in self._LONG_LEVEL_TO_SHORT:
+            raise LogLevelDoesNotExist(level)
+        self._level = self._LONG_LEVEL_TO_SHORT[level]
+
         while True:
             try:
                 self.on_modified(event=None)
@@ -51,17 +59,17 @@ class Reader(FileSystemEventHandler):
             raise LogPathInsufficientPermissions(path=dir)
 
 
-    def tail(self, lines: int):
+    def tail(self, length: int):
         """Emulate tail command behavior by printing n last lines.
 
         Args:
-            lines (int): Number of lines to print
+            length (int): Number of lines to print
         """
 
-        pos = lines + 1
+        pos = length + 1
         rows = []
         with open(self._file) as f:
-            while len(rows) <= lines:
+            while len(rows) <= length:
                 try:
                     f.seek(-pos, 2)
                 except IOError:
@@ -71,11 +79,36 @@ class Reader(FileSystemEventHandler):
                     rows = list(f)
                 pos *= 2
 
-        colored_lines = map(self.color_line, rows[-lines:])
+        return rows[-length:], len(rows)
+
+    def get_output(self, length: int):
+        """Emulate tail command behavior by printing n last lines.
+
+        Args:
+            length (int): Number of lines to print
+        """
+
+        rows, total_rows = [], length + 1
+        if self._level == "DBUG":
+            rows, _ = self.tail(length=length)
+        else:
+            while len(rows) < length and total_rows > length:
+                rows, total_rows = self.tail(length=length)
+                rows = self.filter_lines(rows)
+                length += 1
+
+        colored_lines = map(self.color_line, rows)
         output = "".join(list(colored_lines))
         system(self._CLEAR_CMD)
         print(output)
 
+
+    def filter_lines(self, lines: list):
+        for i, line in enumerate(lines):
+            level = line[:4]
+            if self._LEVELS[self._level] >= self._LEVELS[level]:
+                del lines[i]
+        return lines
 
     def color_line(self, line: str):
         level = line[:4]
@@ -94,7 +127,8 @@ class Reader(FileSystemEventHandler):
         """
 
         print(Style.RESET_ALL, end="")
-        self.tail(get_terminal_size(fallback=(120, 50))[1])
+        self.get_output(get_terminal_size(fallback=(120, 50))[1])
+        #self.get_output(5)
         print(Style.RESET_ALL, end="")
 
 
