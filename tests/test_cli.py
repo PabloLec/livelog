d515cc432a7bed3490f35494740a8b4067e2d11d
PabloLec <pablo.lecolinet@gmail.com>
2021-10-02 14:33:19 +0200

    Complete CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,5 +3,35 @@ from subprocess import Popen, PIPE
 
 def test_default(default_log_file):
     process = Popen("timeout 2 python3 -m livelog".split(), stdout=PIPE)
-    out, err = process.communicate()
+    out, _ = process.communicate()
     assert "It works" in str(out)
+
+
+def test_custom_file(reader_test_file):
+    process = Popen(
+        f"timeout 2 python3 -m livelog -f {reader_test_file}".split(), stdout=PIPE
+    )
+    out, _ = process.communicate()
+    assert "debug" in str(out)
+
+
+def test_nocolors(reader_test_file):
+    process = Popen(
+        f"timeout 2 python3 -m livelog -f {reader_test_file} --nocolors".split(),
+        stdout=PIPE,
+    )
+    out, _ = process.communicate()
+    assert "DBUG" in str(out)
+    assert "INFO" in str(out)
+    assert "WARN" in str(out)
+    assert "ERR!" in str(out)
+
+
+def test_custom_level(reader_test_file):
+    process = Popen(
+        f"timeout 2 python3 -m livelog -f {reader_test_file} --level=INFO --nocolors".split(),
+        stdout=PIPE,
+    )
+    out, _ = process.communicate()
+    assert "INFO" in str(out)
+    assert "DBUG" not in str(out)
