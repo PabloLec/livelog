70595a0b49b61cef886dfe65d3b9d0568d717ea7
PabloLec <pablo.lecolinet@gmail.com>
2021-10-02 11:06:36 +0200

    Clean and add doctrings to `__main__`

--- livelog/__main__.py
+++ livelog/__main__.py
@@ -1,24 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from sys import argv
 from platform import system
 from tempfile import gettempdir
 from pathlib import Path
 from argparse import ArgumentParser
 from livelog.reader import start_reader
-from livelog.logger import Logger
 
 
-def parse_args():
+def _parse_args():
+    """Parse CLI arguments.
+
+    Returns:
+        tuple: Provided arguments
+    """
+
     parser = ArgumentParser(description="Live read a log file")
-    parser.add_argument("-f", "--file", action="store", type=str, required=False)
     parser.add_argument(
-        "-l", "--level", action="store", type=str, default="DEBUG", required=False
+        "-f",
+        "--file",
+        action="store",
+        type=str,
+        required=False,
+        help="Log file to be read",
     )
-    parser.add_argument("--nocolors", action="store_true", required=False)
+    parser.add_argument(
+        "-l",
+        "--level",
+        action="store",
+        type=str,
+        default="DEBUG",
+        required=False,
+        help="Minimum log level. Default: DEBUG",
+    )
+    parser.add_argument(
+        "--nocolors",
+        action="store_true",
+        required=False,
+        help="Do not color lines",
+    )
     args = parser.parse_args()
-
-    print(args)
 
     if args.file is not None:
         file = Path(args.file)
@@ -33,6 +53,5 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    file, level, nocolors = parse_args()
-
+    file, level, nocolors = _parse_args()
     start_reader(file=file, level=level, nocolors=nocolors)
